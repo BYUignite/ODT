2bf45ef7b9c45d8f4cccb187c6d332cfd362d81e
Victoria Stephens <victoria.b.stephens@gmail.com>
2020-02-21 15:42:12 -0600

    Reconstituting radiation code

---
+++ source/radiation/rad_opthin.h
@@ -0,0 +1,49 @@
+/**
+ * @file rad_opthin.h
+ * Header file for class radiation
+ */
+
+#pragma once
+
+#include "radiation.h"
+#include <vector>
+#include <string>
+
+class domain;
+
+using namespace std;
+
+///////////////////////////////////////////////////////////////////////////////
+
+/** Class implementing radiation models: optically thin or two flux.
+ *  Assumes gray gases
+ *
+ */
+
+class rad_opthin : virtual public radiation {
+
+  ////////////////////// DATA MEMBERS /////////////////////
+
+    public:
+
+    ////////////////////// MEMBER FUNCTIONS  /////////////////////
+
+    public:
+
+        virtual void getRadHeatSource(const vector<vector<double> > &xMoleSp,
+                                      const vector<double>          &temp,
+                                      const double                  &pressure,
+                                      const vector<double>          &fvSoot,
+                                      vector<double>                &radSource);
+    private:
+
+    ////////////////////// CONSTRUCTOR FUNCTIONS  /////////////////////
+
+    public:
+
+        rad_opthin(domain *p_domn) : radiation(p_domn) {};   // constructor
+
+        virtual ~rad_opthin(){}
+
+};
+
---
+++ source/radiation/radiation.h
@@ -0,0 +1,72 @@
+/**
+ *  @file radiation.h
+ *  Header file for class radiation
+ */
+
+#pragma once
+
+#include <vector>
+#include <string>
+#include "radiationProperties.h"
+
+class domain;
+
+using namespace std;
+
+// TODO:
+// add pressure to the getRadHeatSource call in enthalpy variable
+// need a radiative property class object
+// use constructor instead of init function here.
+// verify destructor for child classes for radProps
+// add soot radiation
+
+///////////////////////////////////////////////////////////////////////////////
+/** Class implementing radiation models
+ *
+ *  Base class function for various RTE solution methods.
+ *
+ *  Child classes:
+ *      rad_opthin.h    OPTHIN: optically thin approximation
+ *      rad_twoflux.h   TWOFLUX: two-flux approximation, i.e. Schuster-Schwarzchild approximation
+ *      rad_exact.h     EXACT: exact analytical solution with no scattering
+ *      rad_fvdom.h     FVDOM: finite volume discrete ordinates method
+ *      rad_raytrace.h  RAYTRACE: ray tracing solution
+ *
+ *  @author Victoria B. Lansinger
+ *  @author David O. Lignell
+ *
+ */
+
+class radiation {
+
+    ////////////////////// DATA MEMBERS /////////////////////
+
+    public:
+
+        domain                      *domn;          ///< pointer to domain
+        radiationProperties         *radProps;      ///< tools for getting k's and a's, etc. // defines radProps
+
+        double                      sigmaSB;        ///< Stefan Boltzman const
+
+    ////////////////////// MEMBER FUNCTIONS /////////////////////
+
+    public:
+
+        virtual void getRadHeatSource(const vector<vector<double> > &xMoleSp,
+                                      const vector<double>          &temp,
+                                      const double                  &pressure,
+                                      const vector<double>          &fvSoot,
+                                      vector<double>                &radSource) = 0;
+
+    private:
+
+    ////////////////////// CONSTRUCTOR FUNCTIONS /////////////////////
+
+    public:
+
+        radiation(domain *p_domn);   // constructor
+
+        virtual ~radiation(){delete radProps;}          // destructor
+
+};
+
